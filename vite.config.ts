cd3eb71a424e5adeb0ef5fb8c4cdd1b3e9f31d13
Eduarda Menegueli de Souza <eduardasouza111213@gmail.com>
2025-09-09 22:16:23 -0300

    teste 2

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-      }
+      },
+      esbuild: {
+        target: 'esnext',
+      },
     };
 });
