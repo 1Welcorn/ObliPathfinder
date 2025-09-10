e73db5be77c126487f43bd9a8fb76b19cb5e5bf9
Eduarda Menegueli de Souza <eduardasouza111213@gmail.com>
2025-09-09 22:10:08 -0300

    teste 2

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -8,10 +8,11 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  projectId: "obli-fluency-pathfinder",
+  storageBucket: "obli-fluency-pathfinder.firebasestorage.app",
+  messagingSenderId: "361914234340",
+  appId: "1:361914234340:web:12a2f9d022d5bf0e4103f1",
+  measurementId: "G-NTJ1CXRKKH"
 };
 
 // Initialize Firebase
